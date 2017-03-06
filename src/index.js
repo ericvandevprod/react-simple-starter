d19899b11b1f9aea19a0aa8423cc4b3adef154a8
Eric Vandenberg <eric@vandevprod.com>
2017-03-05 21:07:36 -0800

    Search and Video components wired up

--- src/index.js
+++ src/index.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search-bar';
+import VideoList from './components/video-list';
+import VideoDetail from './components/video-detail';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyC2zHCthFRN06NcDOX4f46WZk9pu8Ju4rg';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({
+        selectedVideo: videos[0],
+        videos: videos
+      });
+    });
+  }
+  render() {
+    return (
+        <div>
+          <SearchBar />
+          <VideoDetail video={this.state.selectedVideo} />
+          <VideoList
+              onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+              videos={this.state.videos} />
+        </div>
+    )
+  }
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
